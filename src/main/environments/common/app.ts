f6c98c08bf921bda936e9b06e7206ccc22c4eab7
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-09-13 07:57:22 -0300

    feat: make the project initial setup

---
+++ src/main/environments/common/app.ts
@@ -0,0 +1,22 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { HttpRestEnvironment } from '../http-rest/environment'
+
+export class App {
+    async start(): Promise<void> {
+        const filePath = path.join(__dirname, '..', '..', '..', '..', '.env')
+
+        await fs.access(filePath).catch((_err) => {
+            throw new Error(`File not found: ${filePath}`)
+        })
+
+        this.httpRest().start()
+    }
+
+    private httpRest(): HttpRestEnvironment {
+        return new HttpRestEnvironment(
+            Number(process.env.APP_PORT),
+            String(process.env.APP_VERSION)
+        )
+    }
+}
---
+++ src/main/index.ts
@@ -0,0 +1,4 @@
+import 'dotenv/config'
+import { App } from './environments/common/app'
+
+void new App().start()
