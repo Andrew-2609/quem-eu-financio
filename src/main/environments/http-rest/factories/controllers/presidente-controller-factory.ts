fe10dd4a5ae76398b89866db8fff241b4d348da5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-09-13 11:57:15 -0300

    chore: fix identation

--- src/main/environments/http-rest/factories/controllers/presidente-controller-factory.ts
+++ src/main/environments/http-rest/factories/controllers/presidente-controller-factory.ts
@@ -13,7 +13,9 @@ const makeGetAllPresidentesController = (): RequestHandler => {
 const makeGetPresidenteByNomeController = (): RequestHandler => {
   const controller = new PresidenteController(new PresidenteRepositoryAxios())
   return async (request: Request, response: Response) => {
-    const presidenciavel = await controller.getByNome(request.params['nomePresidente'])
+    const presidenciavel = await controller.getByNome(
+      request.params['nomePresidente']
+    )
     response.json(presidenciavel)
   }
 }
--- src/main/environments/http-rest/routes/presidente-routes.ts
+++ src/main/environments/http-rest/routes/presidente-routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
-import { getAllPresidentes, getPresidenteByNome } from '../factories/controllers/presidente-controller-factory'
+import {
+  getAllPresidentes,
+  getPresidenteByNome
+} from '../factories/controllers/presidente-controller-factory'
 
 export const registerPresidenteRoutes = (router: Router): void => {
   router.get('/presidentes', getAllPresidentes())
