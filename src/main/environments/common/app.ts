1e313603f367b4d00d1ef5b9a57297611090d75c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-09-13 08:15:41 -0300

    feat: add lint configurations and other dev dependencies

--- src/main/environments/common/app.ts
+++ src/main/environments/common/app.ts
@@ -3,20 +3,20 @@ import path from 'path'
 import { HttpRestEnvironment } from '../http-rest/environment'
 
 export class App {
-    async start(): Promise<void> {
-        const filePath = path.join(__dirname, '..', '..', '..', '..', '.env')
+  async start(): Promise<void> {
+    const filePath = path.join(__dirname, '..', '..', '..', '..', '.env')
 
-        await fs.access(filePath).catch((_err) => {
-            throw new Error(`File not found: ${filePath}`)
-        })
+    await fs.access(filePath).catch((_err) => {
+      throw new Error(`File not found: ${filePath}`)
+    })
 
-        this.httpRest().start()
-    }
+    this.httpRest().start()
+  }
 
-    private httpRest(): HttpRestEnvironment {
-        return new HttpRestEnvironment(
-            Number(process.env.APP_PORT),
-            String(process.env.APP_VERSION)
-        )
-    }
+  private httpRest(): HttpRestEnvironment {
+    return new HttpRestEnvironment(
+      Number(process.env.APP_PORT),
+      String(process.env.APP_VERSION)
+    )
+  }
 }
--- src/main/environments/http-rest/environment.ts
+++ src/main/environments/http-rest/environment.ts
@@ -2,27 +2,27 @@ import express, { Router } from 'express'
 import compression from 'compression'
 
 export class HttpRestEnvironment {
-    constructor(
-        private readonly port: number,
-        private readonly version: string
-    ) { }
+  constructor(
+    private readonly port: number,
+    private readonly version: string
+  ) {}
 
-    handler = express()
+  handler = express()
 
-    start(): void {
-        this.handler.use(express.json())
-        this.handler.use(compression())
-        this.handler.disable('x-powered-by')
-        const URL = `/quem-eu-financio/${this.version}`
-        this.handler.use(URL, this.getRouter())
-        this.handler.listen(this.port, () => {
-            console.log(`\n\n\n Listening at ${this.port} \n\n\n`)
-        })
-    }
+  start(): void {
+    this.handler.use(express.json())
+    this.handler.use(compression())
+    this.handler.disable('x-powered-by')
+    const URL = `/quem-eu-financio/${this.version}`
+    this.handler.use(URL, this.getRouter())
+    this.handler.listen(this.port, () => {
+      console.log(`\n\n\n Listening at ${this.port} \n\n\n`)
+    })
+  }
 
-    private getRouter(): Router {
-        const router = Router()
-        router.get('/health', (_req, res) => res.send('ok'))
-        return router
-    }
+  private getRouter(): Router {
+    const router = Router()
+    router.get('/health', (_req, res) => res.send('ok'))
+    return router
+  }
 }
